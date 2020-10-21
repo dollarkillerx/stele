d15624b87416c0585ffe432959d37868e12634bf
dollarkiller <wangy@mvalley.io>
2020-10-21 20:03:53 +0800

    [ADD] grpc api

--- internal/server/stele.go
+++ internal/server/stele.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"context"
+	"errors"
 	"log"
+	"time"
 
 	"github.com/dgraph-io/badger/v2"
 	"github.com/dollarkillerx/stele/internal/config"
@@ -26,13 +28,33 @@ func NewStele(cfg *config.BaseConfig) *SteleServer {
 }
 
 func (s *SteleServer) Set(ctx context.Context, req *generate.SteleKV) (resp *generate.SteleStatus, err error) {
-	s.db.Update(func(txn *badger.Txn) error {
-
+	return nil, s.db.Update(func(txn *badger.Txn) error {
+		entry := badger.NewEntry(req.Key, req.Val)
+		if req.Ttl != 0 {
+			entry = entry.WithTTL(time.Duration(req.Ttl))
+		}
+		return txn.SetEntry(entry)
 	})
 }
 
 func (s *SteleServer) Get(ctx context.Context, req *generate.SteleK) (resp *generate.SteleVal, err error) {
+	resp = &generate.SteleVal{}
+	err = s.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(req.Key)
+		if err != nil {
+			return err
+		}
 
+		return item.Value(func(val []byte) error {
+			resp.Val = val
+			return nil
+		})
+	})
+	if errors.Is(err, badger.ErrKeyNotFound) {
+		return nil, nil
+	}
+
+	return resp, nil
 }
 
 // Batch Insertion Failure Rollback. 批量插入 失败 回滚
--- tc.go
+++ tc.go
@@ -1,13 +1,17 @@
-package script
+package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/dgraph-io/badger/v2"
 	"log"
 	"time"
+
+	"github.com/dgraph-io/badger/v2"
 )
 
 func main() {
+	log.SetFlags(log.Llongfile | log.LstdFlags)
+
 	db, err := badger.Open(badger.DefaultOptions("./badger.db"))
 	if err != nil {
 		log.Fatalln(err)
@@ -21,6 +25,7 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	get(db)
 	// 批量更新
 	batchSet(db)
 	// 设置过期事件
@@ -31,6 +36,23 @@ func main() {
 	prefix(db)
 	// all key
 	allKey(db)
+}
+
+func get(db *badger.DB) {
+	err := db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("scp"))
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		return item.Value(func(val []byte) error {
+			log.Println(string(val))
+			return nil
+		})
+	})
+	log.Println(errors.Is(err, badger.ErrKeyNotFound))
+	//log.Println(errors.As(err, badger.ErrKeyNotFound))
 }
 
 func prefix(db *badger.DB) {
@@ -75,21 +97,24 @@ func batchSet(db *badger.DB) {
 	batch := db.NewWriteBatch()
 	defer batch.Cancel()
 	testP := map[string]string{
-		"sp1":  "sssp1",
-		"sp2":  "sssp2",
-		"sp3":  "sssp3",
-		"sp4":  "sssp4",
+		"sp1":  "sssp1xsxs",
+		"sp2":  "sssp2xsxs",
+		"sp3":  "sssp3xxs",
+		"sp4":  "sssp4xs",
 		"sp5":  "sssp5",
 		"sp6":  "sssp5",
-		"sp7":  "sssp5",
-		"sp8":  "sssp5",
-		"sp9":  "sssp5",
-		"sp10": "sssp5",
-		"sp11": "sssp5",
+		"sp7":  "sssp151515",
+		"sp8":  "sssp5151",
+		"sp9":  "sssp5sdsad",
+		"sp10": "sssp5asdsad",
+		"sp11": "sssp5sds",
 		"sp12": "sssp5",
 		"sp13": "sssp5",
 	}
 	for k, v := range testP {
+		if k == "sp7" {
+			return
+		}
 		if err := batch.Set([]byte(k), []byte(v)); err != nil {
 			log.Println(err)
 		}
